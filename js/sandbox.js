d35e611ef46b63cfcc3f9f332b5cd99a6d02b05a
Denys Volodko <babyleejane@gmail.com>
2022-10-12 21:06:17 +0300

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,16 +1,13 @@
-const products = [
-	{ name: 'Radar', price: 1300, quantity: 4 },
-	{ name: 'Scanner', price: 2700, quantity: 3 },
-	{ name: 'Droid', price: 400, quantity: 7 },
-	{ name: 'Grip', price: 1200, quantity: 9 },
-];
-
-function calculateTotalPrice(productName) {
-	// Change code below this line
-	for (const product of products) {
-		console.log(Object.values(product));
+// Change code below this line
+function findMatches(array, ...args) {
+	const matches = []; // Don't change this line
+	for (const num of array) {
+		if (args.includes(num)) {
+			matches.push(num);
+		}
 	}
 	// Change code above this line
+	return matches;
 }
 
-calculateTotalPrice('Radar');
+console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
