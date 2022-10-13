dca2c34fc240c00263a28bcf32232eae603cc733
Denys Volodko <babyleejane@gmail.com>
2022-10-13 13:47:21 +0300

    update sandbox

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,13 +1,44 @@
-// Change code below this line
-function findMatches(array, ...args) {
-	const matches = []; // Don't change this line
-	for (const num of array) {
-		if (args.includes(num)) {
-			matches.push(num);
+const atTheOldToad = {
+	potions: [
+		{ name: 'Speed potion', price: 460 },
+		{ name: 'Dragon breath', price: 780 },
+		{ name: 'Stone skin', price: 520 },
+	],
+	// Change code below this line
+	getPotions() {
+		return this.potions;
+	},
+	addPotion(newPotion) {
+		for (const potion of this.potions) {
+			if (potion.name === newPotion.name) {
+				return `Error! Potion ${newPotion.name} is already in your inventory!`;
+			}
 		}
-	}
-	// Change code above this line
-	return matches;
-}
+		this.potions.push(newPotion);
+	},
+	removePotion(potionName) {
+		let potionIndex = -1;
+		for (const potion of this.potions) {
+			potionIndex += 1;
+			if (potion.name === potionName) {
+				this.potions.splice(potionIndex, 1);
+			}
+		}
+		return `Potion ${potionName} is not in inventory!`;
+	},
+	updatePotionName(oldName, newName) {
+		let potionIndex = -1;
+		for (const potion of this.potions) {
+			potionIndex += 1;
+			if (potion.name === oldName) {
+				potion.name = newName;
+			}
+		}
+		return `Potion ${oldName} is not in inventory!`;
+	},
+};
+// Change code above this line
 
-console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2));
+// atTheOldToad.addPotion({ name: 'Stone skin', price: 240 });
+atTheOldToad.updatePotionName('Dragon breath', 'Polymorth');
+console.log(atTheOldToad.potions);
