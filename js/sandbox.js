3f12abcf60ab2bc1908b6350ba53a223ed4411a9
Denys Volodko <babyleejane@gmail.com>
2022-11-23 11:19:42 +0200

    update

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,31 +1,3 @@
-//Напиши функцию конструктор User для создания пользователя со следующими свойствами
-//a. userName - имя, строка
-//b. age - возраст, число
-//c. numbersOfPost - количество постов, число
-//d. класс ожидает 1 параметр - объект настроек с одноименными свойствами
+const filter_list = l => l.filter(value => typeof value === 'number');
 
-//Добавь метод getInfo(), который возвращает строку:
-//`Пользователю ${} ${} лет и у него ${} публикаций.`
-
-const User = function (userObj) {
-	const { userName, age, numberOfPost } = userObj;
-	this.userName = userName;
-	this.age = age;
-	this.numberOfPost = numberOfPost;
-	// this.getInfo = function () {
-	// 	console.log(`Користувачу ${userName} ${age} років і він має ${numberOfPost} постів.`);
-	// };
-};
-
-User.prototype.getInfo = function () {
-	console.log(`Користувачу ${this.userName} ${this.age} років і він має ${this.numberOfPost} постів.`);
-};
-
-const poly = new User({
-	userName: 'Poly',
-	age: 33,
-	numberOfPost: 567,
-});
-
-console.log(poly);
-poly.getInfo();
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
