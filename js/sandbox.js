852a8de92bc6fbefb01d9d4e80c11945f4366ecf
Denys Volodko <babyleejane@gmail.com>
2023-02-21 20:42:36 +0200

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,3 +1,23 @@
-const filter_list = l => l.filter(value => typeof value === 'number');
+function towerBuilder(nFloors) {
+	const tower = [];
+	for (let i = 1; i <= nFloors; i++) {
+		tower.push(floorBuilder(nFloors - i, i * 2 - 1));
+	}
+	return tower;
+}
 
-console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
+function floorBuilder(spaces, stars) {
+	const floor = [];
+	for (let m = 1; m <= spaces; m++) {
+		floor.push(' ');
+	}
+	for (let m = 1; m <= stars; m++) {
+		floor.push('*');
+	}
+	for (let m = 1; m <= spaces; m++) {
+		floor.push(' ');
+	}
+	return floor.join('');
+}
+
+console.log(towerBuilder(2));
