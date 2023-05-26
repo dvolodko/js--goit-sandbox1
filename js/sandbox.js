ea80dff2aa8bf39a4eea713b2d4708f5edcff23d
Denys Volodko <babyleejane@gmail.com>
2023-05-26 22:12:51 +0300

    Update sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,62 +1,15 @@
-function rot13(str) {
-	const capitalLettersArray = [
-		'A',
-		'B',
-		'C',
-		'D',
-		'E',
-		'F',
-		'G',
-		'H',
-		'I',
-		'J',
-		'K',
-		'L',
-		'M',
-		'N',
-		'O',
-		'P',
-		'Q',
-		'R',
-		'S',
-		'T',
-		'U',
-		'V',
-		'W',
-		'X',
-		'Y',
-		'Z',
-	];
-	const lettersArray = [
-		'a',
-		'b',
-		'c',
-		'd',
-		'e',
-		'f',
-		'g',
-		'h',
-		'i',
-		'j',
-		'k',
-		'l',
-		'm',
-		'n',
-		'o',
-		'p',
-		'q',
-		'r',
-		's',
-		't',
-		'u',
-		'v',
-		'w',
-		'x',
-		'y',
-		'z',
-	];
-
-	console.log(capitalLettersArray[12]);
+function duplicateEncode(word) {
+	const array = word.split('');
+	const newArray = [];
+	const duplicates = array.filter((item, index) => array.indexOf(item) !== index);
+	array.map(symbol => {
+		if (duplicates.includes(symbol)) {
+			newArray.push(')');
+		} else {
+			newArray.push('(');
+		}
+	});
+	return newArray.join('');
 }
 
-console.log(rot13('"EBG13 rknzcyr."'));
+console.log(duplicateEncode('recede'));
